14a306a7b0a8079dbe9bfac752e03a32fd7cb391
vikram-square-bits <vikram.singh@squarebits.com>
2024-01-02 11:28:43 +0530

    remove 18nModule

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,24 +1,38 @@
+import { MailerModule } from '@nestjs-modules/mailer'
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { I18nModule } from 'nestjs-i18n'
+import { MongooseModule } from '@nestjs/mongoose'
 import * as path from 'path'
 import config from '../config'
+import { UserCollectionName, UserEntity, UserSchema } from './entities/users.entity'
 import { NotificationsController } from './notifications.controller'
 import { NotificationsService } from './services/notifications.service'
-import { MongooseModule } from '@nestjs/mongoose'
-import { UserCollectionName, UserEntity, UserSchema } from './entities/users.entity'
-
+import { join } from 'path'
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
-    I18nModule.forRootAsync({
-      imports: [ConfigModule],
+    MailerModule.forRootAsync({
+      imports: [ConfigModule], // Import the ConfigModule
       useFactory: async (configService: ConfigService) => ({
-        fallbackLanguage: configService.get<string>('language'),
-        loaderOptions: {
-          path: path.join(__dirname, '../common/backend-texts/'),
-          watch: true,
+        transport: {
+          host: configService.get<string>('mailer.transport.host'),
+          port: configService.get<number>('mailer.transport.port'),
+          auth: {
+            user: configService.get<string>('mailer.transport.auth.user'),
+            pass: configService.get<string>('mailer.transport.auth.pass'),
+          },
         },
+        defaults: {
+          from: configService.get<string>('mailer.defaults.from'),
+        },
+        // template: {    // add your custom template for email
+        //   dir: join(__dirname, './templates/'),
+        //   adapter: new HandlebarsAdapter(),
+        //   options: {
+        //     strict: true,
+        //   },
+        // },
       }),
       inject: [ConfigService],
     }),
